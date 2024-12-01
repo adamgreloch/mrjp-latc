a14e9eee0bad115100fc18022a9ab37e0a1adc1b
Adam Greloch <zplhatesbananas@gmail.com>
2024-12-01 15:58:19 +0100

    fir: refactor, first steps towards cfg

--- FIR.hs
+++ FIR.hs
@@ -21,7 +21,7 @@ import Data.Map qualified as M
 -- | Note: String cannot be an Imm, since it has to be allocated
 data VType = VInt | VStr | VBool | VVoid deriving (Show)
 
-type Addr = Int
+data Addr = Cmp Int | Var Ident Int | Temp Int deriving (Show)
 
 data Loc
   = -- | immediate integer literal
@@ -60,11 +60,55 @@ type AddrTypes = Map Addr VType
 -- TODO: code will probably need to be more structured to differentiate basic blocks
 type Code = [Op]
 
-data TopDefFIR = FnDefFIR String Code deriving (Show)
+data TopDefFIR = FnDefFIR String Code
+
+instance (Show TopDefFIR) where
+  show (FnDefFIR s code) =
+    "\n" ++ s ++ foldr (\op acc -> "\n\t" ++ show op ++ acc) "\n" (reverse code)
 
 type ProgramFIR = [TopDefFIR]
 
-data Store = Store_ {locs :: Map SLoc Loc, lastTemp :: Addr, code :: Code} deriving (Show)
+type Label = Int
+
+data BB = BB
+  { label :: Label,
+    stmts :: [Stmt],
+    preds :: [BB],
+    succs :: [BB]
+  }
+  deriving (Show)
+
+type CFG = M.Map Label BB
+
+newBB :: Label -> [Stmt] -> BB
+newBB label stmts =
+  BB {label, stmts, preds = [], succs = []}
+
+addBlockToCFG :: (MonadState Store m) => BB -> m ()
+addBlockToCFG bb = do
+  st <- get
+  put (st {cfg = M.insert (label bb) bb (cfg st)})
+  return ()
+
+addPred :: (MonadState Store m) => Label -> BB -> m ()
+addPred label pred = do
+  st <- get
+  let g = cfg st
+  case M.lookup label g of
+    Nothing -> error "no block with such label in cfg"
+    Just bb -> do
+      let newbb = bb {preds = pred : preds bb}
+      put (st {cfg = M.insert label bb g})
+      return ()
+
+data Store = Store_
+  { locs :: Map SLoc Loc,
+    lastTemp :: Int,
+    lastLabel :: Label,
+    code :: Code,
+    cfg :: CFG
+  }
+  deriving (Show)
 
 class Emittable a where
   emit :: (MonadState Store m) => a -> m ()
@@ -77,6 +121,7 @@ data Op1
   = Call
 
 -- | Binary operands
+-- TODO add signed/unsigned
 data Op2
   = Add
   | Sub
@@ -91,10 +136,17 @@ data Op
   | RetVoid
   | Ret Loc
   | Alloca Addr
+  | EntryLabel
+  | Label Int
+  | CondBr Loc Label Label
+  | Br Label
   deriving (Show)
 
 data Meta
   = FnDecl String Code
+
+instance (Emittable Code) where
+  emit c = modify (\st -> st {code = c ++ code st})
 
 instance (Emittable Meta) where
   emit _ = return ()
@@ -108,9 +160,16 @@ takeCode = do
   st <- get
   let res = code st
   put (st {code = []})
-  return (reverse res)
+  return res
 
 -- Printable needs to know type table
+-- instance (Printable Op2) where
+--   printCode op =
+--     case op of
+--       Add -> "add"
+--       Sub -> "sub"
+--       Mul -> "mul"
+--       FIR.Div -> "div"
+--
 -- instance (Printable Op) where
 --   printCode (BinOp a o l1 l2) = printCode a ++ " = " ++ printCode o ++ printCode l1 ++ printCode l2
-
--- TransformAbsToFIR.hs
+++ TransformAbsToFIR.hs
@@ -35,7 +35,7 @@ data FIRTranslationError' a
 
 type FIRTranslationError = FIRTranslationError' BNFC'Position
 
-type Env = Map Ident SLoc
+data Env = Env {slocs :: Map Ident SLoc, currBB :: Label}
 
 type GenM a = ExceptT FIRTranslationError (StateT Store (Reader Env)) a
 
@@ -47,31 +47,45 @@ freshTemp = do
   st <- get
   let fresh = lastTemp st + 1
   put (st {lastTemp = fresh})
+  return (Temp fresh)
+
+freshVar :: Ident -> GenM Addr
+freshVar idt = do
+  env <- ask
+  loc <- maybeGetLoc idt
+  case loc of
+    (Just (LAddr _ (Var _ n))) -> return (Var idt (n + 1))
+    _else -> return (Var idt 0)
+
+freshLabel :: GenM Label
+freshLabel = do
+  st <- get
+  let fresh = lastLabel st + 1
+  put (st {lastLabel = fresh})
   return fresh
 
-getLoc :: Ident -> GenM Loc
-getLoc idt = do
-  env <- ask
-  case M.lookup idt env of
-    Just sloc -> do
-      st <- get
-      maybe onErr return (M.lookup sloc (locs st))
-    Nothing -> onErr
-  where
-    onErr :: GenM Loc
-    onErr = throwError (UncaughtFrontendErr "No such location")
+maybeGetSLoc :: Ident -> GenM (Maybe SLoc)
+maybeGetSLoc idt = asks (M.lookup idt . slocs)
+
+getSLoc :: Ident -> GenM SLoc
+getSLoc idt = do
+  sl <- maybeGetSLoc idt
+  maybe (error "no such idt") return sl
 
 maybeGetLoc :: Ident -> GenM (Maybe Loc)
 maybeGetLoc idt = do
   env <- ask
-  case M.lookup idt env of
-    Just sloc -> do
-      st <- get
-      maybe onErr (return . Just) (M.lookup sloc (locs st))
+  sloc <- maybeGetSLoc idt
+  case sloc of
+    (Just n) -> gets (M.lookup n . locs)
     Nothing -> return Nothing
-  where
-    onErr :: GenM (Maybe Loc)
-    onErr = throwError (UncaughtFrontendErr "No such location")
+
+getLoc :: Ident -> GenM Loc
+getLoc idt = do
+  env <- ask
+  sloc <- getSLoc idt
+  st <- get
+  maybe (error "no such sloc") return (M.lookup sloc (locs st))
 
 genExp :: Expr -> GenM Loc
 genExp (ELitInt _ n) = return (LImmInt (fromIntegral n))
@@ -86,49 +100,73 @@ genExp (EAdd _ e1 (Plus _) e2) = do
   emit $ BinOp tmp Add loc1 loc2
   return (LAddr (typeOfLoc loc1) tmp)
 genExp (EVar _ id) = getLoc id
+genExp (ELitTrue _) = return (LImmBool True)
 
-genStmt :: Stmt -> GenM Env
-genStmt (Decl _ tp items) = do
-  readerSeq declareItem items
+genStmts :: [Stmt] -> GenM Env
+genStmts [] = ask
+genStmts (Decl _ tp items : t) = do
+  env' <- readerSeq declareItem items
+  local (const env') (genStmts t)
   where
     declareItem :: Item -> GenM Env
     declareItem (NoInit _ idt) = do
       env <- ask
       sloc <- newSLoc
       modify (\st -> st {locs = M.insert sloc (LImmInt 0) (locs st)})
-      return $ M.insert idt sloc env
+      mapIdtToSloc idt sloc
     declareItem (Init _ idt e) = do
       loc <- genExp e
       -- TODO assert expr type of loc is the same as vtp
       env <- ask
       sloc <- newSLoc
       modify (\st -> st {locs = M.insert sloc loc (locs st)})
-      return $ M.insert idt sloc env
-genStmt (Ass _ idt e) = do
+      mapIdtToSloc idt sloc
+genStmts (Ass _ idt e : t) = do
   loc <- genExp e
-  idloc <- getLoc idt
-  case idloc of
-    (LAddr _ addr) -> emit $ Store loc addr
-    _otherwise -> throwError (UncaughtFrontendErr "Assigning to non-addr")
-  ask
-genStmt (SExp _ e) = do
+  sloc <- getSLoc idt
+  modify (\st -> st {locs = M.insert sloc loc (locs st)})
+  genStmts t
+genStmts (BStmt _ b : t) = do
+  code <- genBlock b
+  emit code
+  genStmts t
+genStmts (SExp _ e : t) = do
   _ <- genExp e
-  ask
-genStmt (VRet _) = do
+  genStmts t
+genStmts (VRet _ : t) = do
   emit RetVoid
-  ask
-genStmt (AbsLatte.Ret _ e) = do
+  genStmts t
+genStmts (AbsLatte.Ret _ e : t) = do
   loc <- genExp e
   emit $ FIR.Ret loc
-  ask
+  genStmts t
+genStmts (Cond _ e s : t) = do
+  loc <- genExp e
+  lab1 <- freshLabel
+  lab2 <- freshLabel
+  emit $ CondBr loc lab1 lab2
+  emit $ Label lab1
+  _ <- genStmts [s]
+  emit $ Br lab2
+  emit $ Label lab2
+  genStmts t
+
+currBBLabel :: GenM Label
+currBBLabel = asks currBB
+
+mapIdtToSloc :: Ident -> SLoc -> GenM Env
+mapIdtToSloc idt sloc = do
+  env <- ask
+  return $ env {slocs = M.insert idt sloc (slocs env)}
 
 genBlock :: Block -> GenM Code
 genBlock (Block _ stmts) = do
-  _ <- readerSeq genStmt stmts
+  _ <- genStmts stmts
   takeCode
 
 genTopDefFIR :: TopDef -> GenM TopDefFIR
 genTopDefFIR (FnDef _ _ (Ident s) args block) = do
+  emit EntryLabel
   env' <- readerSeq addArg args
   code <- local (const env') (genBlock block)
   -- FIXME No emit here?
@@ -139,15 +177,27 @@ genTopDefFIR (FnDef _ _ (Ident s) args block) = do
       st <- get
       sloc <- newSLoc
       put (st {locs = M.insert sloc (LArg (toVType tp) argname) (locs st)})
-      asks (M.insert argid sloc)
+      mapIdtToSloc argid sloc
 
 genProgramFIR :: Program -> GenM ProgramFIR
 genProgramFIR (Program _ topdefs) = mapM genTopDefFIR topdefs
 
 runGenM :: GenM a -> (Either FIRTranslationError a, Store)
-runGenM m = runReader (runStateT (runExceptT m) newStore) M.empty
+runGenM m = runReader (runStateT (runExceptT m) emptyStore) emptyEnv
   where
-    newStore = Store_ {locs = M.empty, lastTemp = 0, code = []}
+    emptyStore =
+      Store_
+        { locs = M.empty,
+          lastTemp = 0,
+          lastLabel = 0,
+          code = [],
+          cfg = M.empty
+        }
+    emptyEnv =
+      Env
+        { slocs = M.empty,
+          currBB = 0
+        }
 
 transformAbsToFIR :: Program -> (Either FIRTranslationError ProgramFIR, Store)
 transformAbsToFIR p = runGenM (genProgramFIR p)
