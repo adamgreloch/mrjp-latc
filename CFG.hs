1aa15f77e9dcc0515fccb380f197ce0c9fcbcd86
Adam Greloch <zplhatesbananas@gmail.com>
2024-12-04 11:21:47 +0100

    cfg: track succession reasons

--- CFG.hs
+++ CFG.hs
@@ -14,6 +14,7 @@ import Control.Monad.State
     modify,
     runState,
   )
+import Data.Bifunctor qualified
 import Data.List
 import Data.Map (Map)
 import Data.Map qualified as M
@@ -22,6 +23,14 @@ import Data.Text (pack, replace, unpack)
 import PrintLatte (printTree)
 
 type Label = Int
+
+data When = WhenTrue | WhenFalse | WhenLoop | WhenDone
+
+instance Show When where
+  show WhenTrue = "True"
+  show WhenFalse = "False"
+  show WhenLoop = "Loop"
+  show WhenDone = "Done"
 
 data Node = FnEntry Ident | FnBlock Label | FnRet deriving (Eq)
 
@@ -34,7 +43,7 @@ data BB = BB
   { label :: Label,
     stmts :: [Stmt],
     preds :: [Node],
-    succs :: [Node]
+    succs :: [(Node, When)]
   }
   deriving (Show)
 
@@ -90,13 +99,13 @@ putStmtsToBB label stmts = do
   let bb' = bb {stmts = stmts}
   modify (\st -> st {cfg = M.insert label bb' (cfg st)})
 
-addEdgesFromTo :: [Label] -> Label -> CFGM ()
-addEdgesFromTo labs bb = mapM_ (`addEdgeFromTo` bb) labs
+addEdgesFromTo :: [Label] -> Label -> When -> CFGM ()
+addEdgesFromTo labs bb w = mapM_ (\l -> addEdgeFromTo l bb w) labs
 
 replaceRefToLabel :: Label -> Label -> Node -> CFGM ()
 replaceRefToLabel labFrom labTo (FnBlock lab) = do
   bb <- getBB lab
-  let bb' = bb {preds = map repl (preds bb), succs = map repl (succs bb)}
+  let bb' = bb {preds = map repl (preds bb), succs = map (Data.Bifunctor.first repl) (succs bb)}
   st <- get
   let cfg' = M.insert lab bb' $ cfg st
   put (st {cfg = cfg'})
@@ -112,18 +121,18 @@ mapLabelToBB lab bb = modify (\st -> st {cfg = M.insert lab bb $ cfg st})
 removeLabel :: Label -> CFGM ()
 removeLabel lab = modify (\st -> st {cfg = M.delete lab $ cfg st})
 
-addEdgeFromTo :: Label -> Label -> CFGM ()
-addEdgeFromTo lab0 lab1 = do
+addEdgeFromTo :: Label -> Label -> When -> CFGM ()
+addEdgeFromTo lab0 lab1 w = do
   bb0 <- getBB lab0
   bb1 <- getBB lab1
   if null (stmts bb0) && null (succs bb0)
     then do
       mapM_ (replaceRefToLabel lab0 lab1) (preds bb0)
-      mapM_ (replaceRefToLabel lab0 lab1) (succs bb0)
+      mapM_ (\(l, w) -> replaceRefToLabel lab0 lab1 l) (succs bb0)
       mapLabelToBB lab1 $ bb1 {stmts = stmts bb1 ++ stmts bb0, preds = preds bb0 ++ preds bb1}
       removeLabel lab0
     else do
-      mapLabelToBB lab0 $ bb0 {succs = FnBlock lab1 : succs bb0}
+      mapLabelToBB lab0 $ bb0 {succs = (FnBlock lab1, w) : succs bb0}
       mapLabelToBB lab1 $ bb1 {preds = FnBlock lab0 : preds bb1}
 
 addEntryEdgeTo :: Label -> Ident -> CFGM ()
@@ -134,10 +143,10 @@ addEntryEdgeTo lab fnname = do
   let cfg' = M.insert lab bb' $ cfg st
   put (st {cfg = cfg'})
 
-addRetEdgeFrom :: Label -> CFGM ()
-addRetEdgeFrom lab = do
+addRetEdgeFrom :: Label -> When -> CFGM ()
+addRetEdgeFrom lab w = do
   bb <- getBB lab
-  let bb' = bb {succs = FnRet : succs bb}
+  let bb' = bb {succs = (FnRet, w) : succs bb}
   st <- get
   let cfg' = M.insert lab bb' $ cfg st
   put (st {cfg = cfg'})
@@ -172,7 +181,7 @@ procStmts currLab (stmt : t) =
       currStmts <- takeCurrStmts
       putStmtsToBB currLab currStmts
       lab1 <- freshLabel
-      addEdgeFromTo currLab lab1
+      addEdgeFromTo currLab lab1 WhenTrue
       retLabs <- procStmts lab1 [condstmt]
       if null retLabs
         then -- there's no returning paths from the block, meaning
@@ -180,7 +189,8 @@ procStmts currLab (stmt : t) =
           return []
         else do
           lab <- freshLabel
-          addEdgesFromTo (currLab : retLabs) lab
+          addEdgeFromTo currLab lab WhenFalse
+          addEdgesFromTo retLabs lab WhenDone
           procStmts lab t
     _else -> do
       addStmtToCurrBlock stmt
@@ -191,7 +201,7 @@ procStmts currLab (stmt : t) =
       addStmtToCurrBlock s
       currStmts <- takeCurrStmts
       putStmtsToBB currLab currStmts
-      addRetEdgeFrom currLab
+      addRetEdgeFrom currLab WhenDone
       return []
 
 procBlock :: Block -> CFGM ()
@@ -260,7 +270,7 @@ toDot cfg =
         ++ "\n|"
         ++ (pstr . reverse) (stmts bb)
         ++ "\\l\\\n}\"];\n\n"
-        ++ foldr (\s acc -> showL (label bb) ++ " -> " ++ show s ++ ";\n" ++ acc) [] (succs bb)
+        ++ foldr (\(s,w) acc -> showL (label bb) ++ " -> " ++ show s ++ "[label=" ++ show w ++ "];\n" ++ acc) [] (succs bb)
         ++ foldr (\p acc -> addOnlyEntry p (label bb) acc) [] (preds bb)
     addOnlyEntry :: Node -> Label -> String -> String
     addOnlyEntry fne@(FnEntry _) lab acc = show fne ++ " -> " ++ showL lab ++ ";\n"
